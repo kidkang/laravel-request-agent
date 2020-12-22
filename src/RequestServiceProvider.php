fd5b56d16d5ad5a750867a2c85cacc5438fc74a3
kidkang <723616880@qq.com>
2020-12-22 15:58:35 +0800

    bl

---
+++ src/RequestServiceProvider.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Yjtec\Request\Agent;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\
+class RequestServiceProvider extends ServiceProvider{
+
+    public function register(){
+        dd(123);
+    }
+}
