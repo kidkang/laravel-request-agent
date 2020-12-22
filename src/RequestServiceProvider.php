88ce8f3cc7f54d27d1433a2ea20c11367cfdd7bf
kidkang <723616880@qq.com>
2020-12-22 16:50:59 +0800

    add ismobile iswechat isDingtalk

--- src/RequestServiceProvider.php
+++ src/RequestServiceProvider.php
@@ -1,9 +1,24 @@
 <?php 
 namespace Yjtec\Request\Agent;
 use Illuminate\Support\ServiceProvider;
+use Request;
 class RequestServiceProvider extends ServiceProvider{
 
     public function register(){
-        return 123;
+        Request::macro('agent',function(){
+            return app('agent');
+        });
+
+        Request::macro('isMobile',function(){
+            return app('agent')->isMobile();
+        });
+
+        Request::macro('isWechat',function(){
+            return app('agent')->is('wechat');
+        });
+
+        Request::macro('isDingtalk',function(){
+            return app('agent')->match('DingTalk');
+        });
     }
 }
